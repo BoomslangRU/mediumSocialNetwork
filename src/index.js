706c10c7d54e8ded1144264d50125da4b7232ed3
BoomslangRU <eilinals@mail.ru>
2021-08-17 16:08:12 +0300

    add the top menu

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
+import TopBar from './components/TopBar'
 import Routes from './Routes'
 
 
 const App = () => {
   return <div className="App">
     <BrowserRouter>
+      <TopBar />
       <Routes />
     </BrowserRouter>
   </div>
